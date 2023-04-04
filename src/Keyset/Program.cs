39e665227df9281acd92fc95c440508e96f25149
furkandeveloper <furkan.gungor@mobiroller.com>
2023-04-05 00:51:57 +0300

    🔨 Add test data class and generate test data method

---
+++ src/Keyset/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+static List<TestData> GenerateTestData(int count)
+{
+    var testData = new List<TestData>();
+    for (int i = 0; i < count; i++)
+    {
+        testData.Add(new TestData { Id = Guid.NewGuid(), Value = i });
+    }
+    return testData;
+}
+
+
+class TestData
+{
+    public Guid Id { get; set; }
+    public int Value { get; set; }
+}
