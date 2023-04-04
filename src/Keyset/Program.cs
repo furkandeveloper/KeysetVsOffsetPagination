90f45c5eb2372b830eaa0071df9fe70c9d371842
furkandeveloper <furkan.gungor@mobiroller.com>
2023-04-05 01:09:37 +0300

    🔨 Add PaginationBenchmark class

--- src/Keyset/Program.cs
+++ src/Keyset/Program.cs
@@ -1,21 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
-Console.WriteLine("Hello, World!");
+var summary = BenchmarkRunner.Run<PaginationBenchmark>();
 
-
-
-
-
-static List<TestData> GenerateTestData(int count)
+public class PaginationBenchmark
 {
-    var testData = new List<TestData>();
-    for (int i = 0; i < count; i++)
-    {
-        testData.Add(new TestData { Id = Guid.NewGuid(), Value = i });
-    }
-    return testData;
-}
+    private List<TestData> testData;
+    private int pageSize;
+    private Guid lastId;
 
+    [Params(1000, 10000, 100000)]
+    public int ItemCount { get; set; }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        testData = GenerateTestData(ItemCount);
+        pageSize = 100;
+        lastId = Guid.Empty;
+    }
+
+    [Benchmark]
+    public void KeysetPagination()
+    {
+        var results = new List<TestData>();
+        while (true)
+        {
+            var query = testData.Where(x => x.Id > lastId).OrderBy(x => x.Id).Take(pageSize);
+            var pageResults = query.ToList();
+            results.AddRange(pageResults);
+            if (pageResults.Count < pageSize)
+                break;
+            lastId = pageResults.Last().Id;
+        }
+    }
+
+    [Benchmark]
+    public void OffsetPagination()
+    {
+        var results = new List<TestData>();
+        var offset = 0;
+        while (true)
+        {
+            var query = testData.OrderBy(x => x.Id).Skip(offset).Take(pageSize);
+            var pageResults = query.ToList();
+            results.AddRange(pageResults);
+            if (pageResults.Count < pageSize)
+                break;
+            offset += pageSize;
+        }
+    }
+
+    private List<TestData> GenerateTestData(int count)
+    {
+        var testData = new List<TestData>();
+        for (int i = 0; i < count; i++)
+        {
+            testData.Add(new TestData { Id = Guid.NewGuid(), Value = i });
+        }
+        return testData;
+    }
+}
 
 class TestData
 {
